1a98572be4239a8aa37fb77a2a07b645f7da330a
Artem Sizonenko <admondantes1@gmail.com>
2017-01-18 01:46:27 +0200

    ninth commit

--- js/index.js
+++ js/index.js
@@ -1,20 +1,11 @@
-/**
- * Created by Артем on 05.12.2016.
- */
-
-
-// var clinic, hospital, psyHospital;
-// var clinicArr = [];
-// var hospitalArr = [];
-// var psyArr = [];
-var allArr = [[],[],[]];
+var allArr = [[], [], []];
+var allMarkers;
 function initMap() {
     var centerMap = {lat: 49.6068, lng: 34.5480};
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 6,
         center: centerMap
     });
-//*********************************************
     var posMark = [
         [
             {
@@ -61,7 +52,7 @@ function initMap() {
             img: "img/therapy.png"
         }
     ];
-    var allMarkers = function () {
+    allMarkers = function () {
         for (var j = 0; j < posMark.length; j++) {
             var typeMarker = posMark[j];
             for (var i = 0; i < 2; i++) {
@@ -75,30 +66,22 @@ function initMap() {
             }
         }
     };
-
-
-//*********************************************
-
-
     allMarkers();
 }
 
-
-function remove(nameArr) {
-    for (var i = 0; i < nameArr.length; i++) {
-        nameArr[i].setMap(null);
+function remove(allArrNumber) {
+    for (var i = 0; i < allArrNumber.length; i++) {
+        allArrNumber[i].setMap(null);
     }
 }
-function change(nameId, nameArr, type) {
+function change(nameId, allArrNumber) {
     var checkbox = document.getElementById(nameId);
     if (!checkbox.checked) {
-        remove(nameArr);
+        remove(allArrNumber);
     } else {
-        type();
+        allMarkers();
     }
 }
-
-
 function ok() {
     var look = document.getElementsByClassName("markerController")[0];
     look.classList.toggle("newBook")
